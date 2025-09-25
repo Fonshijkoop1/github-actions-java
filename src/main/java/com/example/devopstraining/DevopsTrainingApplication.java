225e01ba8c8cef697ce02faab3ccfd5d11763cbd
Fonshijkoop1 <fonshijkoop@gmail.com>
2025-09-25 14:32:55 +0200

    Final class maken

--- src/main/java/com/example/devopstraining/DevopsTrainingApplication.java
+++ src/main/java/com/example/devopstraining/DevopsTrainingApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DevopsTrainingApplication {
+final class DevopsTrainingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DevopsTrainingApplication.class, args);
